dee71b548388f8cc67e5f33392651d4d5bce8d15
Daniel Schneider <daschne8@gmail.com>
2019-02-27 01:06:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index { |dwarf,index| puts "#{index+1}. #{dwarf}"  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect {|call| call.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.filter! {|call| call.length > 4}
+  return calls.any?
 end
 
 def find_the_cheese# code an argument here
